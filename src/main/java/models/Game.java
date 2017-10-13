407669f54674815ee69da85c4dc9b588e562deee
SirNexus <aidan.carson@nginx.com>
2017-10-12 18:05:21 -0700

    Remove if

--- src/main/java/models/Game.java
+++ src/main/java/models/Game.java
@@ -16,7 +16,6 @@ public class Game {
 
     public Game(){
         // initialize a new game such that each column can store cards
-        if
         for (int i = 0; i < 4; i++){
             cols.add(i, new ArrayList<Card>());
         }

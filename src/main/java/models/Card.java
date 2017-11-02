6745a863e8795e23d66e89774b5046acd0c8801b
Fauzi Kliman <fauzimerfoo@gmail.com>
2017-11-01 19:12:09 -0700

    Cards on top of columns are displayed as images now, moved background img into images folder, added textRepresentation property to Card class

--- src/main/java/models/Card.java
+++ src/main/java/models/Card.java
@@ -12,12 +12,13 @@ import java.io.Serializable;
 public class Card implements Serializable {
     public final int value;
     public final Suit suit;
+    public final String textRepresentation;
 
     @JsonCreator
     public Card(@JsonProperty("value") int value, @JsonProperty("suit") Suit suit) {
         this.value = value;
         this.suit = suit;
-
+        this.textRepresentation = this.toString();
     }
 
     public Suit getSuit() {
@@ -29,6 +30,20 @@ public class Card implements Serializable {
     }
 
     public String toString() {
-        return this.value + this.suit.toString();
+        String cardValue = Integer.toString(this.value);
+
+        if(this.value == 11)
+            cardValue = "J";
+
+        else if(this.value == 12)
+            cardValue = "Q";
+
+        else if(this.value == 13)
+            cardValue = "K";
+
+        else if(this.value == 14)
+            cardValue = "A";
+
+        return cardValue + " " + this.suit.toString();
     }
 }

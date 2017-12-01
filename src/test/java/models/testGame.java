7ed8ee8fea492e0775a683d95db520256c2ab992
laurengastineau <gastinel@oregonstate.edu>
2017-11-30 17:45:53 -0800

    Merge conflict resolved.

--- src/test/java/models/testGame.java
+++ src/test/java/models/testGame.java
@@ -10,27 +10,27 @@ public class testGame {
 
     @Test
     public void testGameCreation(){
-        Game g = new Game();
+        Game g = new  Game();
         assertNotNull(g);
     }
 
     @Test
     public void testBuildDeck(){
-        Game g = new Game();
+        Game g =  new  Game();
         assertEquals(52,g.deck.cards.size());
     }
 
     @Test
     public void testCardColumnCreation(){
-        Game g = new Game();
+        Game g = new  Game();
         assertEquals(4, g.cardColumns.size());
     }
 
     @Test
     public void testGameShuffle(){
-        Game g1 = new Game();
+        Game g1 = new  Game();
         g1.deck.shuffle();
-        Game g2 = new Game();
+        Game g2 = new  Game();
         g2.deck.shuffle();
         // g1 and g2 could shuffle to the same order, but that chance is approximately 1 in 8*10^67 shuffles
         assertFalse(Arrays.equals(g1.deck.cards.toArray(),g2.deck.cards.toArray()));
@@ -38,7 +38,7 @@ public class testGame {
 
     @Test
     public void testRemoveFunction(){
-        Game g = new Game();
+        Game g = new  Game();
         g.dealFour();
         g.remove(0);
         assertEquals(0, g.cardColumns.get(0).cards.size());
@@ -46,7 +46,7 @@ public class testGame {
 
     @Test
     public void testMove(){
-        Game g = new Game();
+        Game g = new  Game();
         g.dealFour();
         g.move(0, 1);
         assertEquals(2, g.cardColumns.get(1).cards.size());
@@ -54,13 +54,12 @@ public class testGame {
     }
 
     @Test
-    public void testDealFour(){
-
-    }
-
-    @Test
-    public void testAddCardToCol(){
-
+    public void testUpdateScore(){
+        Game g = new  Game();
+        assertEquals(0, g.score);
+        g.dealFour();
+        g.remove(0);
+        assertEquals(1, g.score);
     }
 
 }
